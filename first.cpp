e286074a2817f7cb8398ac9446c590332a1261e3
Aj-mathL <abhijitmajumder545@gmail.com>
2024-09-05 11:25:29 +0530

    Make first cpp program to show how it work

---
+++ first.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std; 
+
+int main(){
+
+    int a, b; 
+    //Here I take two integer. Now we will find sum of those integers
+    cout<<"Enter two number: "; 
+    cin>>a>>b; 
+    cout<<"Sum of "<<a << " and " << b<< "= " <<a + b <<endl; 
+    // Here cout take for output and cin for input 
+    return 0;
+}
